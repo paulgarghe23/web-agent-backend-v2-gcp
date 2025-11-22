0060f6b8ebf280273e10d3f265a0db3f6bf42acc
Paul Garghe <paulgarghe23@gmail.com>
2025-11-22 15:01:33 +0100

    Increase max_tokens in client to test responses

--- app/agent.py
+++ app/agent.py
@@ -47,7 +47,7 @@ def _get_client() -> ChatVertexAI:
             project=project_id,
             location=location,
             temperature=0.2,
-            max_tokens=300,
+            max_tokens=400,
         )
     return _client
 
